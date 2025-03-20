be41373192bdf08b1d2d9ea8874c41f5a1ff07d5
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-20 18:22:43 +0000

    solve ChangeString exercises

---
+++ LP1Semanao3/ChangeString/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static char c;
+
+    private static void Main(string[] args)
+    {
+        // Variables
+        string inputString;
+        char inputChar;
+
+        // Ask user for a string
+        Console.Write("write a string:  ");
+        inputString = Console.ReadLine();
+
+        // Ask user for a character 
+        Console.Write(" write a char:  ");
+        // inputChar = Convert.ToChar(Console.ReadLine());
+        // inputChar = char.Parse(Console.ReadLine());
+        inputChar = Console.ReadLine()[0];
+
+        // write the string without the character
+        foreach (char c in inputString) ;
+        {
+            if (c == inputChar) ;
+            {
+                Console.Write(c);
+            }
+        }
+        Console.WriteLine();
+
+    }
+}
