afc9b7fc05c80bbad07a60a8b6a2d84d7c48f7a3
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-23 13:26:21 +0000

    NPCs have abbilities i guess

---
+++ LP1Semanao3/NPCPerks/Perks.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HeroPerk
+{
+    [Flags]
+    enum Perks
+    {
+
+        WarpShift = 1 << 0,
+        Stealth = 1 << 1,
+        AutoHeal = 1 << 2,
+        DoubleJump = 1 << 3
+    }
+}
---
+++ LP1Semanao3/NPCPerks/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization.Formatters;
+using HeroPerk;
+
+internal class Program
+{
+    private static Perks[] npcPerks;
+    private static int n;
+    private static string? input;
+    private static Perks perks;
+
+    private static void Main(string[] args)
+    {
+        // Variables
+        int numNPCs;
+
+        // Ask for the number of NPCs
+        Console.Write("How many NPC?");
+        numNPCs = int.Parse(Console.ReadLine());
+
+        // Create the array of NPC perks
+        npcPerks = new Perks[numNPCs];
+
+        //For each NPC, ask fot its perks
+        for (int n = 0; n < numNPCs; n++);
+        {
+           // WarpShift?
+           Console.WriteLine($"Does NPC {n} has WarpShift (y/n)? ");
+           input = Console.ReadLine();
+           if (input == "y")
+           {
+             npcPerks[n] |= Perks.WarpShift;
+           }
+
+           //Stealth?
+           Console.WriteLine($"Does NPC {n} has Stealth (y/n)? ");
+           input = Console.ReadLine();
+           if (input == "y")
+           {
+             npcPerks[n] |= Perks.Stealth;
+           }
+
+           //AutoHeal?
+           Console.WriteLine($"Does NPC {n} has AutoHeal (y/n)? ");
+           input = Console.ReadLine();
+           if (input == "y")
+           {
+             npcPerks[n] |= Perks.AutoHeal;
+           }
+
+           //DoubleJump?
+           Console.WriteLine($"Does NPC {n} has DoubleJump (y/n)? ");
+           input = Console.ReadLine();
+           if (input == "y")
+           {
+             npcPerks[n] |= Perks.DoubleJump;
+           }
+        }
+
+        // Show on screen the perks of all NPCs
+        // If NPC has stealth & autoheal print special message
+        foreach (Perks perks in npcPerks);
+        {
+            Perks special = Perks.Stealth | Perks.AutoHeal;
+            Console.WriteLine($"NPC has {perks}");
+            if ((perks & special) == special)
+            {
+                Console.WriteLine("\tHeal silently");
+            } 
+        }
+
+    }
+}
